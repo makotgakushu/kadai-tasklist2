13d25ebefbf04b9bd67dbe51954caff693fadcfb
makot <makot.gakushu@gmail.com>
2017-09-17 11:03:44 +0000

    seventhcommit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit]
   
   def index
     @tasks =Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -13,13 +13,16 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
   
   private
   
+  def set_task
+  @task = Task.find(params[:id])
+  end
+
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 
 end
